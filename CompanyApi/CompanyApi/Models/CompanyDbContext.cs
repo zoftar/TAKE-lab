d9493c78ea7f1ca16f76936cace69086b86880d1
Zosia <zosiarrf@gmail.com>
2020-05-19 01:01:20 +0200

    finish part 1

---
+++ CompanyApi/CompanyApi/Models/CompanyDbContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CompanyApi.Models
+{
+    public partial class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext()
+        {
+        }
+
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Department> Department { get; set; }
+        public virtual DbSet<Employee> Employee { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.Property(e => e.Name).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+
+                entity.HasOne(d => d.Department)
+                    .WithMany(p => p.Employee)
+                    .HasForeignKey(d => d.DepartmentId)
+                    .HasConstraintName("FK_Employee_Department");
+
+                entity.HasOne(d => d.Manager)
+                    .WithMany(p => p.Subordinate)
+                    .HasForeignKey(d => d.ManagerId)
+                    .HasConstraintName("FK_Employee_Employee");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ CompanyApi/CompanyApi/Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CompanyApi.Models
+{
+    public partial class Department
+    {
+        public Department()
+        {
+            Employee = new HashSet<Employee>();
+        }
+
+        [Key]
+        public int DepartmentId { get; set; }
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [InverseProperty("Department")]
+        public virtual ICollection<Employee> Employee { get; set; }
+    }
+}

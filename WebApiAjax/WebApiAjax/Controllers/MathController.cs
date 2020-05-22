9c9b53dddf3c9c9fd49efdea9c6613458b72feed
Zosia <zosiarrf@gmail.com>
2020-05-22 21:58:31 +0200

    finish initial part of lab 10

---
+++ WebApiAjax/WebApiAjax/Controllers/MathController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiAjax.Models;
+
+namespace WebApiAjax.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        // GET api/math
+        [HttpGet]
+        public CalcResult Calculate(int x, int y)
+        {
+            return new CalcResult()
+            {
+                Sum = x + y,
+                Difference = x - y,
+                Product = x * y,
+                Quotient = x / y
+            };
+        }
+    }
+
+
+}
---
+++ WebApiAjax/WebApiAjax/Models/CalcResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiAjax.Models
+{
+    public class CalcResult
+    {
+        public int Sum { get; set; }
+        public int Difference { get; set; }
+        public int Product { get; set; }
+        public int Quotient { get; set; }
+    }
+}
